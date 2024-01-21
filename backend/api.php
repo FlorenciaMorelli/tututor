0a40a413763ca3f8d6da94318074727eb2dfaaf9
Florencia Morelli <florenciamorelliit@gmail.com>
2024-01-21 15:39:03 -0300

    [BE] fix: pequeñas modificaciones a api.php

--- backend/api.php
+++ backend/api.php
@@ -1,20 +1,21 @@
 <?php
-//include "/../config/config.php";
-
 require_once(__DIR__.'/../config/config.php');
 
-/* $metodo = strtolower($_SERVER['REQUEST_METHOD']);
-$accion = explode('/', strtolower($_GET['accion']));
-$funcionNombre = $metodo . ucfirst($accion[0]);
+$metodo = strtolower($_SERVER['REQUEST_METHOD']);
+$accion = isset($_GET['accion']) ? explode('/', strtolower($_GET['accion'])) : [];
+$funcionNombre = $metodo . (isset($accion[0]) ? ucfirst($accion[0]) : '');
 $parametros = array_slice($accion, 1);
-if (count($parametros) >0 && $metodo == 'get') {
+
+if (count($parametros) > 0 && $metodo == 'get') {
     $funcionNombre = $funcionNombre.'ConParametros';
 }
+
 if (function_exists($funcionNombre)) {
-    call_user_func_array ($funcionNombre, $parametros);
+    call_user_func_array($funcionNombre, $parametros);
 } else {
     outputError(400);
-} */
+}
+
 
 function outputJson($data, $codigo = 200)
 {
@@ -53,6 +54,28 @@ function getUsuarios()
     $ret = [];
     while ($fila = mysqli_fetch_assoc($result)) {
         settype($fila['id'], 'integer');
+        $ret[] = $fila;
+    }
+    mysqli_free_result($result);
+    mysqli_close($db);
+    outputJson($ret);
+}
+
+function getAlumnos(){
+    $db = conectarBD();
+    $sql = "SELECT m.id_materia, m.nombre, COUNT(am.id_alumno) AS cantidad_alumnos
+        FROM materias m
+        JOIN alumnos_materias am ON m.id_materia = am.id_materia
+        GROUP BY m.id_materia
+        ORDER BY cantidad_alumnos DESC
+        LIMIT 5";
+    $result = mysqli_query($db, $sql);
+    if ($result===false) {
+        print mysqli_error($db);
+        outputError(500);
+    }
+    $ret = [];
+    while ($fila = mysqli_fetch_assoc($result)) {
         $ret[] = $fila;
     }
     mysqli_free_result($result);
--- config/config.php
+++ config/config.php
@@ -16,11 +16,11 @@ function conectarBD()
     return $link;
 }
 
-function sf__restablecerSql () {
+/* function sf__restablecerSql () {
 	$sqls = array_map(function ($v) {return trim($v);}, explode(PHP_EOL, file_get_contents(__DIR__ . '/../storage/dump.sql')));
 	$nuevoSql = implode(PHP_EOL, array_slice($sqls, array_search('-- #####CORTE#####', $sqls)));
 	return $nuevoSql;
-}
+} */
 
 function postRestablecer () {
     $db = conectarBD();
