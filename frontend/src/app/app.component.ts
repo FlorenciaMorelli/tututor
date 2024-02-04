e55ef194585e891fb3fad93b0eff918436fe5fa9
Florencia Morelli <florenciamorelliit@gmail.com>
2024-02-04 17:34:40 -0300

    [FE] feature: cambio el título de la app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  titulo = "TuTutor.com";
 }
