733f5e5abb61a5d2ac980891085cabd1fd963b28
Florencia Morelli <florenciamorelliit@gmail.com>
2024-02-06 13:51:42 -0300

    [FE] feature: card group para materias

--- frontend/src/app/banner/banner.component.ts
+++ frontend/src/app/banner/banner.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
+import { CardgroupMateriasComponent } from "../cardgroup-materias/cardgroup-materias.component";
 
 @Component({
   selector: 'banner',
   standalone: true,
-  imports: [NavbarComponent, SearchbarComponent],
+  imports: [NavbarComponent, SearchbarComponent, CardgroupMateriasComponent],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
---
+++ frontend/src/app/cardgroup-materias/cardgroup-materias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cardgroup-materias',
+  standalone: true,
+  imports: [],
+  templateUrl: './cardgroup-materias.component.html',
+  styleUrl: './cardgroup-materias.component.css'
+})
+export class CardgroupMateriasComponent {
+
+}
