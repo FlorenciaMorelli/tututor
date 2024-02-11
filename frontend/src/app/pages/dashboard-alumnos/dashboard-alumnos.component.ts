2813d9f6bfc75b4df8626dd528d89ad64b871108
Florencia Morelli <110637670+FlorenciaMorelli@users.noreply.github.com>
2024-02-10 22:49:35 -0300

    [FE] feature: Alumnos Materias (#3)
    * [FE] feature: agrego Angular
    
    * [FE] style: inclusion de bootstrap
    
    * [FE] feature: modificacion de idioma y titulo en index.html
    
    * [FE] fix: modifico favicon.ico al icono de libro de TuTutor.com
    
    * [FE] style: color scheme global para el modo oscuro
    
    * [FE] feature: elimino default
    
    * [FE] feature: agrego inclusiones de bootstrap en index.html
    
    * [FE] feature: cambio el título de la app
    
    * [FE] tests: pruebas de creacion de componente y uso de Decoradores
    
    * [FE] feature: agregando navbar
    
    * [FE] feature: completando elementos del banner
    
    * [FE] style: arreglo estilos
    
    * [FE] feature: card group para materias
    
    * [FE] feature: cardgroup-materias + service materias
    
    * [BE] tests: testeo de postLogin
    
    * [BE] feature: postLogin
    
    * [FE] feature: login con falla de comunicacion, tengo la sospecha que falla desde Angular, por problema del CORS y Apache
    
    * [FE] feature: InicioComponent y ruteo
    
    * [FE] refactor: agrego carpeta components y separo de pages
    
    * [FE] feature: login y signup en modal
    
    * [BE] feature: icono por defecto para las materias
    
    * [BD] feature: agregado de path para iconos de materias en reemplazo de los placeholders
    
    * [FE] feature: agrego imagenes de materias
    
    * [FE] feature: offcanvas y dashboardAlumno
    
    * [FE] style: offcanvas
    
    * [BE] feature: getAlumnosmateriasConParametros
    
    * [BE] tests: pruebo getAlumnosmateriasConParametros($id)
    
    * [FE] feature: servicio Alumnos-Materias
    
    * [FE] feature: Home Alumnos en Dashboard Alumnos

--- frontend/src/app/pages/dashboard-alumnos/dashboard-alumnos.component.ts
+++ frontend/src/app/pages/dashboard-alumnos/dashboard-alumnos.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { OffcanvasComponent } from '../../components/offcanvas/offcanvas.component';
+import { HomeAlumnosComponent } from '../../components/home-alumnos/home-alumnos.component';
+import { SearchbarComponent } from '../../components/searchbar/searchbar.component';
 
 @Component({
   selector: 'dashboard-alumnos',
   standalone: true,
-  imports: [OffcanvasComponent],
+  imports: [OffcanvasComponent, HomeAlumnosComponent, SearchbarComponent],
   templateUrl: './dashboard-alumnos.component.html',
   styleUrl: './dashboard-alumnos.component.css'
 })
---
+++ frontend/src/app/services/alumnos-materias.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnosMateriasService {
+  private apiURL: string;
+
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost/tututor/backend/alumnosMaterias/4'/* + 'id' */;
+  }
+  getAlumnosMaterias() {
+    return this.http.get(this.apiURL);
+  }
+}
