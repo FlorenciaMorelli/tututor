98c23aa24e82821ac055d7ae7354f67c7fe49807
Florencia Morelli <florenciamorelliit@gmail.com>
2024-02-19 19:41:34 -0300

    feature: [FE] uso de Interface

---
+++ frontend/src/app/helpers/interfaces/alumnos.ts
@@ -0,0 +1,11 @@
+export interface Alumnos {
+    //TODO: JSON to TS
+    id_alumno: number,
+    id_usuario: number,
+    nombre: string,
+    apellido: string,
+    zona: string,
+    direccion: string,
+    foto_path: string,
+    puntuacion: number
+}
